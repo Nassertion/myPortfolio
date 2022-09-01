25657398729d97437e189b89fd8f7e75b8c8992e
Nassertiongit config --global user.email www.naser2002@gmail.com <www.naser2002@gmail.com>
2022-09-01 14:32:42 +0300

    message

--- pro.js
+++ pro.js
@@ -1,6 +1,6 @@
 let body = document.querySelector(".main");
 let dark = document.querySelector(".dark");
-console.log(body);
+
 dark.addEventListener('click',()=>{
     dark.classList.toggle('active');
     body.classList.toggle('night');
